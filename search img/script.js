a5544bc0ce7df21836dfbf0bf8c46bfb8c47b3ba
devkotabipin42 <devkotabipin42@gmail.com>
2025-11-24 14:57:08 +0900

    upgrade

---
+++ search img/script.js
@@ -0,0 +1,45 @@
+const  accessKey ='x3yskC5jVlktbDpAeemfji2YtS8RFGX5_Fo7JF5ue2E'
+
+const searchEngine = document.querySelector('.search-engine')
+const searchBox = document.getElementById('search-box')
+const searchResult = document.getElementById('search-result')
+const showMoreBtn = document.querySelector('.show-more-btn')
+
+let keyword = ''
+let page = 1
+
+async function searchImage(){
+ keyword= searchBox.value
+ const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`
+ 
+ const response= await fetch(url)
+ const data =await response.json()
+
+ if(page===1){
+  searchResult.innerHTML=''
+ }
+ const results = data.results
+
+ results.map((result)=>{
+  const image = document.createElement('img')
+  image.src = result.urls.small
+  const imageLink = document.createElement('a')
+  imageLink.href = result.links.html
+  imageLink.target= '_blank'
+  imageLink.appendChild(image)
+  searchResult.appendChild(imageLink)
+  console.log(data)
+ })
+ showMoreBtn.style.display = 'block'
+}
+
+searchEngine.addEventListener('submit',(e)=>{
+ e.preventDefault()
+ page = 1
+ searchImage()
+})
+
+showMoreBtn.addEventListener('click',()=>{
+ page++
+ searchImage()
+})
